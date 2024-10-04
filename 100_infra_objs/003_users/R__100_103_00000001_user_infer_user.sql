9981d3550bea5d0d533c540c5324304f50af0c9e
nkipi999 <neeraj.s.kumar@kipi.ai>
2024-10-04 12:26:23 +0530

    4-10-2

--- 100_infra_objs/003_users/R__100_103_00000001_user_infer_user.sql
+++ 100_infra_objs/003_users/R__100_103_00000001_user_infer_user.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE USER {{ env }}_INFER_SCHEMACHANGE_USER
 MUST_CHANGE_PASSWORD = TRUE
 PASSWORD = 'ABC'
-COMMENT = 'This is a dummy uat  user for SDI schemachange tasks';
+COMMENT = 'This is a dummy uat user for SDI schemachange tasks';
