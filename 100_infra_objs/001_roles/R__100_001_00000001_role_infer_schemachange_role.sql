9a11c562592d8facdea95492449815663c9de73a
Neeraj kumar <54062423+nky2j@users.noreply.github.com>
2024-11-11 22:33:04 +0530

    Update R__100_001_00000001_role_infer_schemachange_role.sql

--- 100_infra_objs/001_roles/R__100_001_00000001_role_infer_schemachange_role.sql
+++ 100_infra_objs/001_roles/R__100_001_00000001_role_infer_schemachange_role.sql
@@ -4,5 +4,7 @@ GRANT USAGE ON WAREHOUSE {{ env }}_SCHEMACHANGE_WH TO ROLE {{ env }}_INFER_SCHEM
 */
 {% if env == "DEV" %}
   CREATE ROLE  IF NOT EXISTS {{ env }}_INFER_SCHEMACHANGE_ROLE;
+{% else %}
+  Select "Any env other than dev does not need developer_func role.";
 {% endif %}
 
